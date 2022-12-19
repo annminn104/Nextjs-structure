e396e0c048a21968826fb96e728b341ead3ef6ed
annminn100 <annminn100@gmail.com>
2022-12-19 13:34:18 +0700

    build: implement MUI UI & emotion styled & SSR

--- components/layout/Body.tsx
+++ components/layout/Body.tsx
@@ -1,11 +1,14 @@
 import { IBodyProps } from '@interfaces/layout';
+import { Container } from '@mui/material';
 import React from 'react';
 import styles from './Body.module.scss';
 
 export const Body = ({ children, ...props }: IBodyProps) => {
   return (
     <React.Fragment>
-      <main className={styles.main}>{children}</main>
+      <Container maxWidth="xl" component="main" className={styles.main}>
+        {children}
+      </Container>
     </React.Fragment>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,46 @@
 import { Body, Footer, Header, Wrapper } from '@components/layout';
 import { Seo } from '@components/shared';
+import { CacheProvider, EmotionCache } from '@emotion/react';
 import '@interceptors/request';
 import '@interceptors/response';
+
 import { store } from '@stores/index';
 
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import '../styles/sass/index.scss';
 
-function MyApp({ Component, pageProps }: AppProps) {
+import { createEmotionCache } from '@configs/createEmotionCache';
+import { theme } from '@configs/theme';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
-    <Provider store={store}>
-      <Seo />
-      <Wrapper>
-        <Header></Header>
-        <Body>
-          <Component {...pageProps} />
-        </Body>
-        <Footer></Footer>
-      </Wrapper>
-    </Provider>
+    <CacheProvider value={emotionCache}>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <Seo />
+          <Wrapper>
+            <Header></Header>
+            <Body>
+              {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+              <CssBaseline />
+              <Component {...pageProps} />
+            </Body>
+            <Footer></Footer>
+          </Wrapper>
+        </ThemeProvider>
+      </Provider>
+    </CacheProvider>
   );
 }
 
